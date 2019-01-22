17fa4fd80546590235838b19dbe997e92f4928af
Zach Hamlin <zachhamlin6@gmail.com>
2019-01-22 13:45:27 -0600

    fixed again.

--- radicale_infcloud/__init__.py
+++ radicale_infcloud/__init__.py
@@ -23,6 +23,7 @@ from http import client
 from radicale import storage, web
 
 from radicale.httputils import NOT_FOUND
+
 # from radicale.web import MIMETYPES, FALLBACK_MIMETYPE
 MIMETYPES = {
     ".css": "text/css",
@@ -44,14 +45,45 @@ FALLBACK_MIMETYPE = "application/octet-stream"
 VERSION = "2.0.0"
 
 
-class Web(web.BaseWeb):
+class Web(web.Web):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.infcloud_folder = pkg_resources.resource_filename(__name__, "web")
 
+    def base_get(self, environ, base_prefix, path, user):
+        assert path == "/.web" or path.startswith("/.web/")
+        assert pathutils.sanitize_path(path) == path
+        try:
+            filesystem_path = pathutils.path_to_filesystem(
+                self.folder, path[len("/.web"):].strip("/"))
+        except ValueError as e:
+            logger.debug("Web content with unsafe path %r requested: %s",
+                         path, e, exc_info=True)
+            return httputils.NOT_FOUND
+        if os.path.isdir(filesystem_path) and not path.endswith("/"):
+            location = posixpath.basename(path) + "/"
+            return (client.FOUND,
+                    {"Location": location, "Content-Type": "text/plain"},
+                    "Redirected to %s" % location)
+        if os.path.isdir(filesystem_path):
+            filesystem_path = os.path.join(filesystem_path, "index.html")
+        if not os.path.isfile(filesystem_path):
+            return httputils.NOT_FOUND
+        content_type = MIMETYPES.get(
+            os.path.splitext(filesystem_path)[1].lower(), FALLBACK_MIMETYPE)
+        with open(filesystem_path, "rb") as f:
+            answer = f.read()
+            last_modified = time.strftime(
+                "%a, %d %b %Y %H:%M:%S GMT",
+                time.gmtime(os.fstat(f.fileno()).st_mtime))
+        headers = {
+            "Content-Type": content_type,
+            "Last-Modified": last_modified}
+        return client.OK, headers, answer
+
     def get(self, environ, base_prefix, path, user):
         if not path.startswith("/.web/infcloud/") and path != "/.web/infcloud":
-            status, headers, answer = super().get(environ, base_prefix, path,
+            status, headers, answer = self().base_get(environ, base_prefix, path,
                                                   user)
             if status == client.OK and path in ("/.web/", "/.web/index.html"):
                 answer = answer.replace(b"""\
